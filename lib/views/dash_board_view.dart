d5385f96e6e1e32e61f0f18ec12deb051f88ea7d
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-15 01:23:51 +0200

    creat user infi list tile

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => Center(child: Text('Mobile'),),
+         tabletLayout: (context) => Center(child: Text('Tablet'),),
+          desktopLayout: (context) => Center(child: Text('Desktop'),),
+          
+          )
+    );
   }
 }
