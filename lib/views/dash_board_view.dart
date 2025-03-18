9bbc47c081af3ae2f777ecd7b6bc4f3939cefe81
Ehabhanna1 <ehabhanna1599@gmail.com>
2025-03-18 15:50:38 +0200

    add dash board mobile layout

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
 import 'package:responsive_dash_board/widgets/dash_board_destop_layout.dart';
+import 'package:responsive_dash_board/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dash_board/widgets/dashboard_tablet_layout.dart';
 
 class DashBoardView extends StatelessWidget {
@@ -9,10 +10,16 @@ class DashBoardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: MediaQuery.sizeOf(context).width < 650 ? AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFFFAFAFA),
+        leading: Icon(Icons.menu),
+      ) : null,
       backgroundColor: Color(0xFFF7F9FA),
       body: AdaptiveLayout(
         desktopLayout: (context) => const DashBoardDestopLayout(),
-        mobileLayout: (context) =>  Container(color: Colors.blue,),
+        mobileLayout: (context) =>  DashBoardMobileLayout(),
         tabletLayout: (context) => DashboardTabletLayout(),
        
         
--- lib/widgets/dashboard_tablet_layout.dart
+++ lib/widgets/dashboard_tablet_layout.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/widgets/all_expensess_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
 import 'package:responsive_dash_board/widgets/dashboard_mobile_layout.dart';
-import 'package:responsive_dash_board/widgets/income_section.dart';
-import 'package:responsive_dash_board/widgets/my_card_and_transaction_history_sections.dart';
 
 class DashboardTabletLayout extends StatelessWidget {
   const DashboardTabletLayout({super.key});
@@ -16,7 +13,10 @@ class DashboardTabletLayout extends StatelessWidget {
         const SizedBox(width: 20,),
         Expanded(
           flex: 3,
-          child: DashBoardMobileLayout()),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: DashBoardMobileLayout(),
+          )),
           SizedBox(width: 20,),
       ],
     );
